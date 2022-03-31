fc3e09d3e4bcc5139366d148e7754657eec64cba
catpattie <57093270+catpattie@users.noreply.github.com>
2022-03-31 15:01:25 +0100

    Fixed alert

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -1,54 +1,78 @@
+
+    function CustomAlert() {
+        this.render = function (dialog) {
+            var winW = window.innerWidth;
+            var winH = window.innerHeight;
+            var screenoverlay = document.getElementById('divoverlay');
+            screenoverlay.style.display = "block";
+            var dialogbox = document.getElementById('dialogbox');
+            screenoverlay.style.height = winH + "px";
+            dialogbox.style.left = (winW / 2) - (550 * 0.5) + "px";
+            dialogbox.style.top = "150px";
+            dialogbox.style.display = "block";
+            document.getElementById('dialogboxbody').innerHTML = dialog;
+            document.getElementById('dialogboxfoot').innerHTML = '<button class="ok" onclick="Alert.close()">OK</button>';
+        }
+        this.close = function () {
+            document.getElementById('dialogbox').style.display = "none";
+            document.getElementById('divoverlay').style.display = "none";
+
+        }
+    }
+
+    var Alert = new CustomAlert();
+
 document.addEventListener('DOMContentLoaded', () => {
 
     //card options
     const cardArray = [
     {
         name: 'avocado',
-        img: '../imgs/avocado.png'
+        img: 'imgs/avocado.png'
     },
     {
         name: 'avocado',
-        img: '../imgs/avocado.png'
+        img: 'imgs/avocado.png'
     },
     {
         name: 'donut',
-        img: '../imgs/donut.png'
+        img: 'imgs/donut.png'
     },
     {
         name: 'donut',
-        img: '../imgs/donut.png'
+        img: 'imgs/donut.png'
     },
     {
         name: 'cookies',
-        img: '../imgs/cookies.png'
+        img: 'imgs/cookies.png'
     },
     {
         name: 'cookies',
-        img: '../imgs/cookies.png'
+        img: 'imgs/cookies.png'
     },
     {
         name: 'pizza',
-        img: '../imgs/pizza.png'
+        img: 'imgs/pizza.png'
     },
     {
         name: 'pizza',
-        img: '../imgs/pizza.png'
+        img: 'imgs/pizza.png'
     },
     {
         name: 'lemons',
-        img: '../imgs/lemons.png'
+        img: 'imgs/lemons.png'
     },
     {
         name: 'lemons',
-        img: '../imgs/lemons.png'
+        img: 'imgs/lemons.png'
     },
     {
         name: 'cake',
-        img: '../imgs/cake.png'
+        img: 'imgs/cake.png'
     },
     {
         name: 'cake',
-        img: '../imgs/cake.png'
+        img: 'imgs/cake.png'
     }
 ]
 
@@ -70,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img')
-            card.setAttribute('src', '../imgs/blank.png')
+            card.setAttribute('src', 'imgs/blank.png')
             card.setAttribute('data-id', i)
             card.addEventListener('click', flipcard)
             grid.appendChild(card)
@@ -83,14 +107,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const optionOneID = cardsChosenID[0]
         const optionTwoID = cardsChosenID[1]
         if (cardsChosen[0] === cardsChosen[1]) {
-            alert('You found a match!')
-            cards[optionOneID].setAttribute('src', '../imgs/white.png')
-            cards[optionTwoID].setAttribute('src', '../imgs/white.png')
+            Alert.render('You found a match!')
+            cards[optionOneID].setAttribute('src', 'imgs/white.png')
+            cards[optionTwoID].setAttribute('src', 'imgs/white.png')
             cardsWon.push(cardsChosen)
         } else {
-            cards[optionOneID].setAttribute('src', '../imgs/blank.png')
-            cards[optionTwoID].setAttribute('src', '../imgs/blank.png')
-            alert('Sorry, try again!')
+            cards[optionOneID].setAttribute('src', 'imgs/blank.png')
+            cards[optionTwoID].setAttribute('src', 'imgs/blank.png')
+            Alert.render('Sorry, try again!')
         }
         
         cardsChosen = []
