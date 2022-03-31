9fec727da59ef10476c640601b6408e3746095f3
megchills <meg-campbell@hotmail.com>
2022-03-31 14:43:24 +0100

    linking js file

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -52,68 +52,71 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 ]
 
-//randomise card array
-cardArray.sort(() => 0.5-Math.random())
 
-const grid = document.querySelector('.grid')
-const resultDisplay = document.querySelector('#result')
-const attemptsDisplay = document.querySelector('#attempts')
-var cardsChosen = []
-var cardsChosenID = []
-var cardsWon = []
+ //randomise card array
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    const attemptsDisplay = document.querySelector('#attempts')
+    var cardsChosen = []
+    var cardsChosenID = []
+    var cardsWon = []
 
 
-var attempts = 0
+    var attempts = 0
 
 // create game board
-function createBoard() {
-    for (let i=0; i< cardArray.length; i++){
-        var card = document.createElement('img')
-        card.setAttribute('src', '../imgs/blank.png')
-        card.setAttribute('data-id', i)
-        card.addEventListener('click', flipcard)
-        grid.appendChild(card)
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', '../imgs/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
     }
-}
 
 //check for matches 
-function checkForMatch() {
-    var cards = document.querySelectorAll('img')
-    const optionOneID = cardsChosenID[0]
-    const optionTwoID = cardsChosenID[1]
-    if (cardsChosen[0] === cardsChosen[1]){
-        alert('You found a match!')
-        cards[optionOneID].setAttribute('src', '../imgs/white.png')
-        cards[optionTwoID].setAttribute('src', '../imgs/white.png')
-        cardsWon.push(cardsChosen)
-    } else {
-        cards[optionOneID].setAttribute('src', '../imgs/blank.png')
-        cards[optionTwoID].setAttribute('src', '../imgs/blank.png')
-        alert('Sorry, try again!')
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneID = cardsChosenID[0]
+        const optionTwoID = cardsChosenID[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match!')
+            cards[optionOneID].setAttribute('src', '../imgs/white.png')
+            cards[optionTwoID].setAttribute('src', '../imgs/white.png')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneID].setAttribute('src', '../imgs/blank.png')
+            cards[optionTwoID].setAttribute('src', '../imgs/blank.png')
+            alert('Sorry, try again!')
+        }
+        
+        cardsChosen = []
+        cardsChosenID = []
+
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = 'Congratulations, you found them all!'
+        }
+
+        attempts++
+        attemptsDisplay.textContent = attempts
+        console.log(attempts)
     }
-    cardsChosen = []
-    cardsChosenID = []
-    resultDisplay.textContent = cardsWon.length
-    if (cardsWon.length === cardArray.length/2){
-        resultDisplay.textContent = 'Congratulations, you found them all!'
-    }
-    attempts++
-    attemptsDisplay.textContent = attempts
-    console.log(attempts)
-}
 
 //flip card
-function flipcard() {
-    var cardID = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardID].name)
-    cardsChosenID.push(cardID)
-    this.setAttribute('src', cardArray[cardID].img)
-    if (cardsChosen.length === 2){
-        setTimeout(checkForMatch, 500)
+    function flipcard() {
+        var cardID = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardID].name)
+        cardsChosenID.push(cardID)
+        this.setAttribute('src', cardArray[cardID].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
     }
-}
 
 
-createBoard()
-
+    createBoard()
 })
